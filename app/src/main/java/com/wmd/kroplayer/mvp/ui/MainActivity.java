b5affe6b08db4cd1fb0a6044a3ece3952d5967ed
Edwardwmd <1732141816wmd@gmail.com>
2020-02-18 21:57:31 +0800

    add LauncherActivity,Improve the method of BasePresenter.

--- app/src/main/java/com/wmd/kroplayer/mvp/ui/MainActivity.java
+++ app/src/main/java/com/wmd/kroplayer/mvp/ui/MainActivity.java
@@ -1,17 +1,55 @@
 package com.wmd.kroplayer.mvp.ui;
 
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.wmd.kroplayer.R;
+import com.wmd.kroplayer.base.BaseActivity;
+import com.wmd.kroplayer.mvp.contract.MainContract;
+import com.wmd.kroplayer.mvp.presenter.MainPresenter;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.View {
+
+      @BindView(R.id.fl_content)
+      FrameLayout flContent;
+      @BindView(R.id.tab_bottom_nav)
+      BottomNavigationView tabBottomNav;
 
       @Override
-      protected void onCreate(Bundle savedInstanceState) {
+      protected void initActivityCompontent() {
 
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+      }
+
+      @Override
+      public void showLoading() {
+
+      }
+
+      @Override
+      public void hideLoading() {
+
+      }
+
+      @Override
+      public void showMessage(String message) {
+
+      }
+
+      @Override
+      public int initView(@Nullable Bundle savedInstanceState) {
+
+            return R.layout.activity_main;
+      }
+
+      @Override
+      public void initData(@Nullable Bundle savedInstanceState) {
+
       }
 }
